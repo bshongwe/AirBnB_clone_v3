4d935eec2ae17dc80270b7cf9e29ce72c0213c6a
bshongwe <dru.da.beat.hustle@gmail.com>
2024-02-26 01:04:53 +0200

    Task 5 - app <revert code>

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,29 +1,44 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 '''
-Creates a Flask app, registers blueprint app_views with the Flask instance 'app'
+This script creates a Flask app and registers blueprint app_views with the Flask instance 'app'
 '''
 
-import os
-from flask import flask, jsonify, make_response
+from os import getenv
+from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
+
+# Task 12
+# Enable CORS and allow requests from any origin:
+CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
+
+# Register the app_views blueprint:
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
-
+# Teardown function to close the SQLAlchemy session object after each request:
 @app.teardown_appcontext
-def call_close(err):
-    """storage close()"""
+def teardown_db(exception):
+    '''
+    Remove the current SQLAlchemy session object after each request.
+    '''
     storage.close()
 
-
+# Task 5
+# Error handler for 404 Not Found:
 @app.errorhandler(404)
-def not_found(err):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+def not_found(error):
+    '''Return JSON response with "Not Found" error message.
+    '''
+    response = {'error': 'Not Found'}
+    return jsonify(response), 404
 
-
-if __name__ == "__main__":
-    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.environ.get('HBNB_API_PORT', '5000'))
-    app.run(host=host, port=port, threaded=True)
+if __name__ == '__main__':
+    # Get the host and port from environment variables.
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000))
+    # Run the app in threaded mode for better performance:
+    app.run(host=HOST, port=PORT, threaded=True)
