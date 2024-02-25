fe2e334c74f2e9203efdd438991c056331493ff1
bshongwe <dru.da.beat.hustle@gmail.com>
2024-02-25 23:47:53 +0200

    Task 3, 4 & 5: API dir <API endpoints & specs>, v1 dir <rGET endpoint setup>, app <404 ---> JSON>

---
+++ api/v1/views/index.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+index: JSON
+"""
+
+
+from models import storage
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', strict_slashes=False)
+def ok():
+    """status ok"""
+    return (jsonify(status="ok"))
+
+@app_views.route('/stats', strict_slashes=False)
+def number_objs():
+    """returns object numbers"""
+    return (jsonify(
+        amenities=storage.count('Amenity'),
+        cities=storage.count('City'),
+        states= storage.count('States'),
+        reviews= storage.count('Review'),
+        places= storage.count('Place'),
+        users= storage.count('User')
+
+        ))
