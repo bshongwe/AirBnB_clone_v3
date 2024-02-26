46d91444ad1252a4bbbcd2baf0f0e0bbb0629de8
NdimmuZiickey <zeeisa518@gmail.com>
2024-02-26 13:50:21 -0800

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,31 +1,35 @@
 #!/usr/bin/python3
-"""
-index
-"""
+'''
+Create a route '/status' on the object app_views
+'''
 
 
-from models import storage
-from api.v1.views import app_views
 from flask import jsonify
+from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def api_status():
-    """status ok"""
+    '''
+    Return a JSON response for RESFYL API health
+    '''
     response = {'status': 'OK'}
     return jsonify(response)
 
 #task 4
 @app_views.route('/status', methods=['GET'])
 def get_status():
-    """retrieve number of each objects by type"""
+    '''
+    retrieve number of each objects by type
+    '''
     status = {
     'amenities': storage.count('Amenity'),
     'cities': storage.count('City'),
     'states': storage.count('States'),
     'reviews': storage.count('Review'),
     'places': storage.count('Place'),
-    'users': storage.count('User')
+    'users': storage.count('User'),
         
         }
     return jsonify(status)
