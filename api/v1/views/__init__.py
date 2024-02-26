e3ada8ab43764f9c3ab19d998023d106440255d1
bshongwe <dru.da.beat.hustle@gmail.com>
2024-02-27 01:13:34 +0200

    Hot fix 1

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-'''
-Create a Blueprint instance with 'url_prefix' set to '/api/v1
-'''
-
+"""__init__"""
 from flask import Blueprint
+
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
@@ -14,4 +12,3 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
-'''
-Create a route '/status' on the object app_views
-'''
+"""
+index
+"""
 
 
-from flask import jsonify
-from api.v1.views import app_views
 from models import storage
+from api.v1.views import app_views
+from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'])
-def api_status():
-    '''
-    Return a JSON response for RESFYL API health
-    '''
-    response = {'status': 'OK'}
-    return jsonify(response)
+@app_views.route('/status', strict_slashes=False)
+def ok():
+    """status ok"""
+    return (jsonify(status="ok"))
 
-#task 4
-@app_views.route('/status', methods=['GET'])
-def get_status():
-    '''
-    retrieve number of each objects by type
-    '''
-    status = {
-    'amenities': storage.count('Amenity'),
-    'cities': storage.count('City'),
-    'states': storage.count('States'),
-    'reviews': storage.count('Review'),
-    'places': storage.count('Place'),
-    'users': storage.count('User'),
-        
-        }
-    return jsonify(status)
+
+@app_views.route('/stats', strict_slashes=False)
+def number_objs():
+    """returns objects numbers"""
+    return (jsonify(
+        amenities=storage.count('Amenity'),
+        cities=storage.count('City'),
+        states=storage.count('States'),
+        reviews=storage.count('Review'),
+        places=storage.count('Place'),
+        users=storage.count('User')
+
+        ))
