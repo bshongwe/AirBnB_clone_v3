f9b5190d8789d8baa64957a6f06f02bd544ffb9a
NdimmuZiickey <zeeisa518@gmail.com>
2024-02-26 13:34:21 -0800

    __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""creates Blueprint instance"""
+'''
+Create a Blueprint instance with 'url_prefix' set to '/api/v1
+'''
 
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
